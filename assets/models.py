76b27e89d61709eed3f87658195af67ba7903603
imoxto <mohammad.rafid.hamid@g.bracu.ac.bd>
2023-09-03 21:04:23 +0600

    basic rest api using templates

--- assets/models.py
+++ assets/models.py
@@ -1,3 +1,20 @@
+
 from django.db import models
 
-# Create your models here.
+class Company(models.Model):
+    name = models.CharField(max_length=100)
+
+class Employee(models.Model):
+    name = models.CharField(max_length=100)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+
+class Device(models.Model):
+    name = models.CharField(max_length=100)
+    condition = models.CharField(max_length=100)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+
+class DeviceLog(models.Model):
+    device = models.ForeignKey(Device, on_delete=models.CASCADE)
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    checked_out = models.DateTimeField()
+    returned = models.DateTimeField(null=True, blank=True)
---
+++ assets/urls.py
@@ -0,0 +1,14 @@
+
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import CompanyViewSet, EmployeeViewSet, DeviceViewSet, DeviceLogViewSet
+
+router = DefaultRouter()
+router.register(r'companies', CompanyViewSet)
+router.register(r'employees', EmployeeViewSet)
+router.register(r'devices', DeviceViewSet)
+router.register(r'devicelogs', DeviceLogViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
